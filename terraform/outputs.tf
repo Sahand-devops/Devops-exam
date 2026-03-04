6dfe92c88817c62457254efe8c9ee05c101595cb
Sahand <sahandelias@live.se>
2026-03-04 14:45:12 +0000

    vm_ip output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,7 @@ output "vm_name" {
 output "vm_state" {
   value = hyperv_machine_instance.vm.state
 }
+
+output "vm_ip" {
+  value = hyperv_machine_instance.vm.network_adaptors[0].ip_addresses[0]
+}
